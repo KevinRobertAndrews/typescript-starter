f4e5829a171a231d469f1ca32e7512d9964b6a6a
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2022-02-20 16:22:05 -0500

    finish basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /**
  * Calculate the average of three numbers
- * 
+ *
  * @param a - first number
  * @param b - second number
  * @param c - third number
  */
-export function average(a: number, b:number, c:number): number {
-    return (a + b + c) / 3;
+export function average(a: number, b: number, c: number): number {
+  return (a + b + c) / 3;
 }
